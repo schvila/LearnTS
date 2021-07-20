f009133e93c337b7c06b17da7899a5b2b6f9b0fd
Stanislav Chvila <schvila@gmail.com>
2021-07-20 11:10:27 +0200

    test date-fns

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,9 @@ import PersonTest from "./persontest";
 import gettersetter from "./department";
 import ITestable from "./itestable";
 import stattest from "./statictest";
+import datetest from "./datefnstest";
 
-let tests: ITestable[] = [new PersonTest(), gettersetter, stattest];
+let tests: ITestable[] = [new PersonTest(), gettersetter, stattest, datetest];
 tests.forEach((test) => {
   console.log("--------------------------------------");
   console.log(`Runing test '${test.name}'`);
---
+++ src/datefnstest.ts
@@ -0,0 +1,24 @@
+import Module from "module";
+import ITestable from "./itestable";
+
+import { format, add, parseJSON } from "date-fns";
+
+
+class DateFnsTest implements ITestable {
+  name = "date-fns library";
+  Run() {
+    console.log(format(new Date(2014, 1, 11), "MM/dd/yyyy"));
+    let d = parseJSON("2021-07-16T04:29:12.339Z");
+    console.log(format(d, "dd.M.yyyy"));
+
+    let d1 = Date.now();
+    let d2 = add(d1{
+        years:2,
+        weeks:1,
+    });
+    console.log(JSON.stringify(d2));
+    // console.log(format(d2, "dd.M.yyyy"));
+  }
+}
+
+export default new DateFnsTest();
